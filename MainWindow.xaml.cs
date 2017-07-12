366888cc7ca3a1b67629f865a42ba137b2731667
TruthOriginem <553816216@qq.com>
2017-07-12 20:15:51 +0800

    最初版本

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using StarsectorLTool.Src.Global;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StarsectorLTool
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (File.Exists(Global.TEST_VM_PATH + "\\" + "vmparams"))
+            {
+                lab_vmTips.Content = "-已检测到vmparams-";
+                lab_vmTips.Foreground = Brushes.Green;
+            }
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
+
+        }
+    }
+}
---
+++ Src/Global/Global.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarsectorLTool.Src.Global
+{
+    static class Global
+    {
+        public const string TEST_VM_PATH = "E:\\Starsector08\\Starsector";
+    }
+}
